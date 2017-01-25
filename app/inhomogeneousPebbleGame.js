f6e584772fcacc26c55ebfb8c640f8de26ac2a21
niole <niolenelson@gmail.com>
2017-01-24 17:43:56 -0800

    creates InhomogeneousPG class and adds run method

--- app/inhomogeneousPebbleGame.js
+++ app/inhomogeneousPebbleGame.js
@@ -15,41 +15,135 @@ import Histogram from './Histogram.js';
  * state which is the outcomes matrix
  * */
 
-function generateWeights(boardSize, minWeight, maxWeight) {
-  const diff = maxWeight - minWeight;
-  let board = new Array(boardSize);
+export default class InhomogenousPG  {
+  constructor(size, minWeight, maxWeight, timeout = 100, totalIterations) {
+    this._hist = new Histogram(size);
+    this._weights = this._generateWeights(size, minWeight, maxWeight);
+    this._transitionMatrix = this._generateTransitionMatrix(size);
+    this._pos = 0;
+    this._timeout = timeout;
+    this._remainingIterations = totalIterations;
+    this.size = size;
 
-  let i=0;
-  for (; i < boardSize; i++) {
-    board[i] = minWeight+Math.floor(diff*Math.random());
+    this.updateHistogram();
   }
 
-  return board;
-}
-
-/**
- * returns the moves matrix
- * */
-export function nextMoveMatrix(boardSize) {
-  const width = Math.sqrt(boardSize);
-
-  assert.isNumber(boardSize, "must be a number");
-  assert.equal(width, Math.round(width),
-    "rounded value must be the same as original square root of boardSize");
-
-  let board = new Array(boardSize);
-  let currHeight = 0;
-
-  let i=0;
-  for (; i < boardSize; i++) {
-    //direction: 0, 1, 2, 3 --> N, E, S, W
-    board[i] = [
-      i+width < boardSize ? i+width : i,
-      (i+1)%width !== 0 ? i+1 : i,
-      i-width > -1 ? i-width : i,
-      i%width !== 0 ? i-1 : i
-    ];
+  updateHistogram() {
+    this._hist.inc(this.getPos());
   }
 
-  return board;
+  decRemainingIterations() {
+    this._remainingIterations -= 1;
+  }
+
+  getRemainingIterations() {
+    return this._remainingIterations;
+  }
+
+  getTimeout() {
+    return this._timeout;
+  }
+
+  run(callback) {
+    //callback is for relaying data back to the visualization
+    //callback will be executed on a timeout
+    const self = this;
+
+    while (this.getRemainingIterations()) {
+      //run simulation again
+      const nextPos = this.getNextPos();
+
+      if (this.shouldMove(this.getPos(), nextPos)) {
+        this.setPos(nextPos);
+      }
+
+      this.updateHistogram();
+
+      setTimeout(() => {
+        callback(self.getHistogram());
+        self.decRemainingIterations();
+      }, this.getTimeout());
+
+    }
+
+  }
+
+  getNextPos() {
+    const neighbors = this.getNeighborsAt(this.getPos());
+    return neighbors[Math.floor(Math.random()*neighbors.length)];
+  }
+
+  setPos(newPos) {
+    this._pos = newPos;
+  }
+
+  getPos() {
+    return this._pos;
+  }
+
+  getWeights() {
+    return this._weights;
+  }
+
+  getWeightAt() {
+    return this._weights;
+  }
+
+  shouldMove(from, to) {
+    const transitionProb = Math.random();
+    return transitionProb <= this.getWeightAt(to)/this.getWeightAt(from);
+  }
+
+  _generateWeights(boardSize, minWeight, maxWeight) {
+    const diff = maxWeight - minWeight;
+    let board = new Array(boardSize);
+
+    let i=0;
+    for (; i < boardSize; i++) {
+      board[i] = minWeight+Math.floor(diff*Math.random());
+    }
+
+    return board;
+  }
+
+  getNeighborsAt(index) {
+    assert.isTrue(index > -1 && index < this.size, "index must be in range");
+
+    return this.getTransitionMatrix()[index];
+  }
+
+  getTransitionMatrix() {
+    return this._transitionMatrix;
+  }
+
+  getHistogram() {
+    return this._hist;
+  }
+
+  /**
+   * returns the moves matrix
+   * */
+  _generateTransitionMatrix(boardSize) {
+    const width = Math.sqrt(boardSize);
+
+    assert.isNumber(boardSize, "must be a number");
+    assert.equal(width, Math.round(width),
+      "rounded value must be the same as original square root of boardSize");
+
+    let board = new Array(boardSize);
+    let currHeight = 0;
+
+    let i=0;
+    for (; i < boardSize; i++) {
+      //direction: 0, 1, 2, 3 --> N, E, S, W
+      board[i] = [
+        i+width < boardSize ? i+width : i,
+        (i+1)%width !== 0 ? i+1 : i,
+        i-width > -1 ? i-width : i,
+        i%width !== 0 ? i-1 : i
+      ];
+    }
+
+    return board;
+  }
 }
